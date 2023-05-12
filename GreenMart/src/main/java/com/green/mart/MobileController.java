8744a76c4ec5790c3d1fa6a479fff9cf7dcd53e9
SYW <SYW@DESKTOP-OPMG77C>
2023-05-12 21:59:35 +0900

    2023-05-12 조용국 모바일 기초

--- GreenMart/src/main/java/com/green/mart/MobileController.java
+++ GreenMart/src/main/java/com/green/mart/MobileController.java
@@ -2,18 +2,28 @@ package com.green.mart;
 
 import java.util.HashMap;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.green.mart.service.MobileService;
+import com.green.mart.vo.CustomerVo;
+
 @Controller
 @RequestMapping("/M")
 public class MobileController {
 	
+	@Autowired
+	private MobileService mobileService;
+	
 	@RequestMapping("/Login_check")
 	public ModelAndView login_check(@RequestParam HashMap<String, String> map){
 		System.out.println(map);
+		
+		CustomerVo vo = new CustomerVo();
+		vo = mobileService.getCustomer(map);
 		ModelAndView mv = new ModelAndView();
 		mv.addObject("message", "error");
 		mv.setViewName("mobile/mhome");
---
+++ GreenMart/src/main/java/com/green/mart/service/MobileService.java
@@ -0,0 +1,11 @@
+package com.green.mart.service;
+
+import java.util.HashMap;
+
+import com.green.mart.vo.CustomerVo;
+
+public interface MobileService {
+
+	CustomerVo getCustomer(HashMap<String, String> map);
+
+}

0c506fbb2ab18e829f165f874900dfb60eae9af1
5 5 <5 5@DESKTOP-B3F6CMS>
2023-05-16 23:28:19 +0900

    dddd

--- GreenMart/src/main/java/com/green/interceptor/AuthInterceptor.java
+++ GreenMart/src/main/java/com/green/interceptor/AuthInterceptor.java
@@ -26,6 +26,12 @@ public class AuthInterceptor extends HandlerInterceptorAdapter{
 		Object obj = session.getAttribute("login");
 		if(obj == null) {
 			// 로그인되어 있지 않다면 /login 으로 이동하시오
+			 String userAgent = request.getHeader("User-Agent");
+		        if (userAgent != null && userAgent.contains("Mobile")) {
+		            response.sendRedirect("/home"); // 홈 URL로 변경해야 함
+		            return false;
+		        }
+			
 			response.sendRedirect("/login");
 			return false;
 		}
@@ -35,12 +41,5 @@ public class AuthInterceptor extends HandlerInterceptorAdapter{
 		return super.preHandle(request, response, handler);
 	}
 	
-	//postHandle() : Controller 가 실행되고 화면이 보여지기 직전에 수행되는 메소드
-	@Override
-	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
-			ModelAndView modelAndView) throws Exception {
-		// TODO Auto-generated method stub
-		super.postHandle(request, response, handler, modelAndView);
-	}
 
 }

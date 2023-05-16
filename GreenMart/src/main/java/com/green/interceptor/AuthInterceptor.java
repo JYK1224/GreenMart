3e2e2058c81702e0f5b88185e8c67ca3190922b1
SYW <SYW@DESKTOP-OPMG77C>
2023-05-17 01:25:12 +0900

    2023-05-17 조용국 인터셉터 수정

--- GreenMart/src/main/java/com/green/interceptor/AuthInterceptor.java
+++ GreenMart/src/main/java/com/green/interceptor/AuthInterceptor.java
@@ -7,46 +7,43 @@ import javax.servlet.http.HttpSession;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
-public class AuthInterceptor extends HandlerInterceptorAdapter{
-	
-	//preHandle() : Controller 보다 먼저 실행되는 메소드
-	@Override
-	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
-			throws Exception {
-		// session 객체를 가져옴
-		HttpSession session = request.getSession();
-		String requestUrl = request.getRequestURL().toString();
-		
-		
-		// /login 페이지는 체크에서 제외해야함
-		if(requestUrl.contains("/login")) {
-			return true;
-		}
-		//--------------------------------
-		Object obj = session.getAttribute("login");
-		if(obj == null) {
-		    // 모바일에서 로그인 페이지로 이동하지 않고 바로 홈으로 이동하도록 처리
-	        String userAgent = request.getHeader("User-Agent");
-	        if (userAgent != null && userAgent.contains("Mobile")) {
-	            response.sendRedirect("/home"); // 홈 URL로 변경해야 함
-	            return false;
-	        }
+public class AuthInterceptor extends HandlerInterceptorAdapter {
+    
+    // preHandle(): Method executed before the Controller
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+            throws Exception {
+        // Get the session object
+        HttpSession session = request.getSession();
+        String requestUrl = request.getRequestURL().toString();
 
-	        response.sendRedirect("/login");
-	        return false;
-	    }
-		
-		//preHandle 의 return 은 컨트롤러 요청 url로 가도 되나 안되나를 허가하는 의미
-		//true : 컨트롤러 url로 가게됨
-		return super.preHandle(request, response, handler);
-	}
-	
-	//postHandle() : Controller 가 실행되고 화면이 보여지기 직전에 수행되는 메소드
-	@Override
-	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
-			ModelAndView modelAndView) throws Exception {
-		// TODO Auto-generated method stub
-		super.postHandle(request, response, handler, modelAndView);
-	}
+        // Exclude "/login" page from checking
+        if (requestUrl.equals("http://localhost:9090/")) {
+            // Check if the user is logged in
+            Object obj = session.getAttribute("login");
+            if (obj == null) {
+                // If not logged in and not accessing "/Mobile", redirect to "/login"
+                response.sendRedirect("/login");
+                return false;
+            }
+        }
 
+        // Check if the request URL contains "/Mobile"
+        if (requestUrl.contains("/Mobile")) {
+            return true; // Skip the interceptor and allow access to "/Mobile"
+        }
+
+
+
+        // By default, continue with the existing logic for other pages
+        return super.preHandle(request, response, handler);
+    }
+    
+    // postHandle(): Method executed after the Controller
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+            ModelAndView modelAndView) throws Exception {
+        // TODO Auto-generated method stub
+        super.postHandle(request, response, handler, modelAndView);
+    }
 }

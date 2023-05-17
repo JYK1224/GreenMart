46ea3a8d3760cccec405c53ff6488a874e5685a9
GGG <GGG@DESKTOP-8N3GG4O>
2023-05-17 11:32:08 +0900

    즐겨찾기 홈컨트롤러 변경

--- GreenMart/src/main/java/com/green/mart/HomeController.java
+++ GreenMart/src/main/java/com/green/mart/HomeController.java
@@ -4,6 +4,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.green.mart.service.FavoriteService;
+import com.green.mart.vo.EmployeeVo;
 import com.green.mart.vo.FavoriteVo;
 import com.green.menu3.service.Menu3Service;
 import com.green.menu3.vo.Menu3PagingVo;
@@ -25,42 +28,44 @@ public class HomeController {
 	@Autowired
 	private FavoriteService favoriteService;
 	
-	@RequestMapping("/")
-	public  ModelAndView  home() {
-		
-		// 홈화면 공지사항 띄우기 : map
-		HashMap<String, Object> map = new HashMap<String, Object>();
-		map.put("nowpage", 1); // 첫페이지를 띄울거다
-		map.put("pagecount", 5); // 5줄 띄울거다 
-		map.put("startnum", 1); // DB에서 1~5 줄 가져올거다
-		map.put("endnum", 5); // DB에서 1~5 줄 가져올거다	
-		map.put("m_id", "M01"); // M01 : 공지사항
-		List<Menu3PagingVo> noticeList = menu3Service.homeNoticeList( map );
-		
-		// 홈화면 업무연락 띄우기 : map2
-		HashMap<String, Object> map2 = new HashMap<String, Object>();
-		map2.put("nowpage", 1); // 첫페이지를 띄울거다
-		map2.put("pagecount", 5); // 5줄 띄울거다 
-		map2.put("startnum2", 1); // DB에서 1~5 줄 가져올거다
-		map2.put("endnum2", 5); // DB에서 1~5 줄 가져올거다	
-		map2.put("m_id2", "M02"); // M02 : 업무연락
-		List<Menu3PagingVo> contactList = menu3Service.homeContactList( map2 );
-		
-		// 즐겨찾기 메뉴 가져오기 : map3
-		HashMap<String, Object> map3 = new HashMap<String, Object>();
-		map3.put("e_id", "0001"); // 로그인아이디를 e_id 로 map에 담는다.
-		List<FavoriteVo> favoriteList = favoriteService.getFavoriteList( map3 );
-		
-		ModelAndView mv = new ModelAndView();
-		mv.setViewName("home");
-		mv.addObject("map", map);
-		mv.addObject("map2", map2);
-		mv.addObject("noticeList", noticeList);
-		mv.addObject("contactList", contactList);
-		mv.addObject("favoriteList", favoriteList);
-		
-		return  mv;
-	}
+	   @RequestMapping("/")
+	   public  ModelAndView  home(HttpSession session) {
+	      
+	      // 홈화면 공지사항 띄우기 : map
+	      HashMap<String, Object> map = new HashMap<String, Object>();
+	      map.put("nowpage", 1); // 첫페이지를 띄울거다
+	      map.put("pagecount", 5); // 5줄 띄울거다 
+	      map.put("startnum", 1); // DB에서 1~5 줄 가져올거다
+	      map.put("endnum", 5); // DB에서 1~5 줄 가져올거다   
+	      map.put("m_id", "M01"); // M01 : 공지사항
+	      List<Menu3PagingVo> noticeList = menu3Service.homeNoticeList( map );
+	      
+	      // 홈화면 업무연락 띄우기 : map2
+	      HashMap<String, Object> map2 = new HashMap<String, Object>();
+	      map2.put("nowpage", 1); // 첫페이지를 띄울거다
+	      map2.put("pagecount", 5); // 5줄 띄울거다 
+	      map2.put("startnum2", 1); // DB에서 1~5 줄 가져올거다
+	      map2.put("endnum2", 5); // DB에서 1~5 줄 가져올거다   
+	      map2.put("m_id2", "M02"); // M02 : 업무연락
+	      List<Menu3PagingVo> contactList = menu3Service.homeContactList( map2 );
+	      
+	      // 즐겨찾기 메뉴 가져오기 : map3
+	      HashMap<String, Object> map3 = new HashMap<String, Object>();
+	      EmployeeVo vo = (EmployeeVo) session.getAttribute("login");
+	      String e_id = vo.getE_id();
+	      map3.put("e_id", e_id); // 로그인아이디를 e_id 로 map에 담는다.
+	      List<FavoriteVo> favoriteList = favoriteService.getFavoriteList( map3 );
+	      
+	      ModelAndView mv = new ModelAndView();
+	      mv.setViewName("home");
+	      mv.addObject("map", map);
+	      mv.addObject("map2", map2);
+	      mv.addObject("noticeList", noticeList);
+	      mv.addObject("contactList", contactList);
+	      mv.addObject("favoriteList", favoriteList);
+	      
+	      return  mv;
+	   }
 	// 헤더 메뉴
 		//--------------------------------------
 	@RequestMapping("/Jumpo")
